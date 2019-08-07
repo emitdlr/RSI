ac4d6e3e0f9447628fceb1992d0703e92a41e339
emitdlr <emit@utexas.edu>
2019-08-06 22:42:11 -0500

    added additional styling and mongodb

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+
+//     $('#collapseExample').on('hide.bs.collapse', function () {
+//        $('#myBtn').removeClass('btn-success');
+//        $('#myBtn').addClass('btn-primary');
+//     });
+    
+//     $('#collapseExample').on('show.bs.collapse	', function () {
+//        $('#myBtn').removeClass('btn-primary');
+//        $('#myBtn').addClass('btn-success');
+//     })
+    
+//     });
